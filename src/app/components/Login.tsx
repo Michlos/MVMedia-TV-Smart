40a19799ca150b26d641d2bbdc6230ee622ea5e0
michlos <michlos@live.com>
2026-03-27 08:45:24 -0300

    fix: configuring login navigation to right position after inserted character in tv controller

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { Tv, MonitorPlay } from "lucide-react";
 import { ImageWithFallback } from "./figma/ImageWithFallback";
-import { useTVNavigation } from "../hooks/useTVNavigation";
+import { useTVNavigation } from "../hook/useTvNavigation";
 
 export function Login() {
   useTVNavigation();
