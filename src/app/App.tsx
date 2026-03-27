184aff6fdd15e23236f5266f5efa33c6d81ba033
michlos <michlos@live.com>
2026-03-27 07:38:26 -0300

    fix: configure tv navigation in app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import useNavigation from "./hook/use-navigation";
 
 export default function App() {
-
-  useNavigation()
-  
   return <RouterProvider router={router} />;
 }
