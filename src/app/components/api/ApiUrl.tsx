5323d45add6b4dd76eed07ed3aac75d9f21aa1e5
michlos <michlos@live.com>
2026-04-01 02:23:04 -0300

    fix: url tumbnail fixed

--- src/app/components/api/ApiUrl.tsx
+++ src/app/components/api/ApiUrl.tsx
@@ -5,6 +5,6 @@ export const API_URLS = {
     LOGIN: `${BASE_URL}api/User/Login`,
     LIST_FILES: `${BASE_URL}api/MediaFile/ListActiveMediaFiles`,
     PLAY_FILE: `${BASE_URL}api/MediaFile/GetToPlay/`, //ACRESENDTAR O ID DO ARQUIVO
-    GET_TUMB: `${BASE_URL}Videos/` //ACRESCENTAR O FILENAME TUMB DO ARQUIVO DE VIDEO
+    GET_TUMB: `${BASE_URL}Videos` //ACRESCENTAR O FILENAME TUMB DO ARQUIVO DE VIDEO
 
 }
